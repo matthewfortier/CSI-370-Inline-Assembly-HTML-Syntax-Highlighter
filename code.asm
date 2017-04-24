c26a3c3fab1efae5d52ad8899d0f22955669a6d8
Matthew Fortier <Matthew Fortier>
2017-04-24 10:56:54 -0400

    Initial Upload

---
+++ code.asm
@@ -0,0 +1,83 @@
+; Assignment 8.2 Orbital Velocity
+; Author: Matthew Fortier
+; Description: Calculates orbital velocity with given distance from the Earth
+; Running MASM Assembler on Windows
+; Using Visual Studio assemble, run, and debug code
+; I certify that this is my own work
+
+_getDouble PROTO						; declare external functions
+_printString PROTO
+_printDouble PROTO
+
+.data
+align 16								; align data on 16-byt boundary
+G			REAL8 6.6741E-11			; constants in scientific notation
+me			REAL8 5.9722E24
+distance    REAL8 6.371E6
+mph			REAL8 0.44704
+exitNum		REAL8 0.0
+radius	    REAL8 0.0
+ovelocity	REAL8 0.0
+										; display messages
+endMessage BYTE "Continue? (1/0): ", 0
+radiusMessage BYTE "Enter distance(m) of object from surface of the Earth: ", 0
+msMessage BYTE "Orbital Velocity(m/s): ", 0
+mphMessage BYTE "Orbital Velocity(mph): ", 0
+
+.code
+_asmMain PROC 
+	
+	push rbp
+	sub rsp, 20h
+	lea rbp, [rsp + 20h]				; align the stack pointer
+
+	velocity:
+
+		lea rcx, radiusMessage			; load string to be printed
+		call _printString				; print string to stdout
+		call _getDouble					; use C++ function to get double
+
+		addsd xmm0, distance			; getDouble puts return value in xmm0, add the radius of the earth
+
+		movsd radius, xmm0				; mov the add result to radius variable
+
+		lea rcx, msMessage
+		call _printString
+
+		movsd xmm0, G					; load gravitational constant
+		mulsd xmm0, me					; multiply it against the mass of the eart
+
+		divsd xmm0, radius				; divide by the calculated radius
+
+		sqrtsd xmm0, xmm0				; square root the result
+
+		movsd ovelocity, xmm0			; mov the result into the appropriate variable
+
+		call _printDouble				; print the results in (m/s)
+
+		lea rcx, mphMessage
+		call _printString				
+
+		movsd xmm0, ovelocity			
+		divsd xmm0, mph					; divide velocity by mph constant
+
+		call _printDouble
+
+		lea rcx, endMessage
+		call _printString
+		call _getDouble
+
+		movsd xmm1, xmm0				; mov the given double to xmm1 for comparison
+		movsd xmm2, exitNum				; compare against 0 constant
+		comisd xmm1, xmm2				; compare the two float registers and set the flags
+		je endProgram					; end if equal
+
+		jmp velocity					; repeat
+
+	endProgram:
+	
+	lea rsp, [rbp]						; clean up stack and end
+	pop rbp
+	ret
+_asmMain ENDP
+END
---
+++ main.cpp
@@ -0,0 +1,175 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <stdio.h>
+#include <unordered_map>
+#include <windows.h>
+using namespace std;
+
+string returnSpan(string input) {
+	return "<span>" + input + "</span>";
+}
+
+string returnSpan(string input, string color, string style) {
+	return "<span style='color:" + color + ";" + style + "'>" + input + "</span>";
+}
+
+string returnSpanCat(string str, string sub, int length, int location) {
+	return str.replace(location, length, sub);
+}
+
+extern "C" void _stdcall debugPrint(char str) {
+	cout << str;
+}
+
+extern "C" void _stdcall printString(char buffer[], int count) {
+	for (int i = 0; i < count; i++) {
+		cout << buffer[i];
+	}
+	cout << endl;
+}
+
+extern "C" void _stdcall checkColor(unordered_map<string, string> &keys, char buffer[], int count) {
+	string temp = "";
+	for (int i = 0; i < count; i++) {
+		temp += buffer[i];
+	}
+
+	if (keys[temp] == "blue") {
+		cout << "<span style='color:" + keys[temp] + ";" + "font-weight: bold" + "'>" + temp + "</span>" << endl;
+	}
+}
+
+vector<string> split(const string &s, char delim) {
+	stringstream ss(s);
+	string item;
+	vector<string> tokens;
+	while (getline(ss, item, delim)) {
+		tokens.push_back(item);
+	}
+	return tokens;
+}
+
+int main() {
+
+	ofstream fout;
+	ifstream fin;
+
+	string temp = "";
+	string substr = "";
+	char format[] = "%s";
+	int lineLength = 0;
+	char* tempAddress;
+	byte found = 0;
+
+	int start = 0;
+	int end = 0;
+	DWORD count = 0;
+	byte buffer[256] = "";
+
+	fin.open("instructions.txt");
+
+	unordered_map<string, string> keys;
+	string instruction;
+	while (!fin.eof()) {
+		getline(fin, instruction);
+		keys[instruction] = "blue";
+	}
+
+	fin.close();
+
+	fin.open("code.asm");
+	fout.open("code.html");
+
+	//fout << "<pre style='color: #000000; background: #ffffff;'>" << endl;
+	//fout << "<h1>Assembly Syntax Highlighting</h1>" << endl;
+
+	while (!fin.eof()) {
+
+		getline(fin, temp);
+		lineLength = temp.length();
+
+		tempAddress = &temp[0];
+
+		char tempChar;
+
+		_asm {
+
+			mov ecx, lineLength
+			cmp ecx, 0
+			je endLine
+
+			mov esi, tempAddress
+
+			parseLine:
+				
+			lodsb
+				mov tempChar, al
+
+				cmp al, '\0'
+				je endLine
+
+				cmp al, '\n'
+				je endLine
+
+				cmp al, ','
+				je endLine
+
+				; cmp al, ';'
+				; je comment
+
+				cmp al, 32
+				je notLetter
+
+				cmp al, '\t'
+				je notLetter
+				
+				lea edi, buffer
+				mov edx, count
+				mov [edi + edx], al
+				inc count
+
+				jmp continueParse
+
+				notLetter:
+					
+					mov eax, count
+					cmp eax, count
+					jnz continueParse
+
+					push count
+					lea eax, buffer
+					push eax
+					lea eax, keys
+					push eax
+					call checkColor
+				
+					mov count, 0
+				
+				continueParse:
+
+				loop parseLine
+
+			endLine:
+			
+			comment:
+
+		}
+
+		cout << endl;
+	}
+
+	//fout << "</pre>" << endl;
+
+	fin.close();
+	fout.close();
+
+	//ShellExecute(NULL, "open", "code.html",
+	//	NULL, NULL, SW_SHOWNORMAL);
+
+	system("pause");
+
+	return 0;
+}
